798981d7f83a77839f94f01f7a9ee19234e01143
xthiagosant <uzmaki_thiago@outlook.com>
2021-08-03 06:11:10 -0300

    criado metodos para criação de um novo post

--- src/main/java/com/tsswebapps/tssblog/controller/BlogController.java
+++ src/main/java/com/tsswebapps/tssblog/controller/BlogController.java
@@ -1,13 +1,18 @@
 package com.tsswebapps.tssblog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.tsswebapps.tssblog.model.Post;
 import com.tsswebapps.tssblog.service.BlogService;
@@ -31,6 +36,22 @@ public class BlogController {
 		Post posts = blogService.findById(id);
 		mAv.addObject("postItem", posts);
 		return mAv;
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.GET)
+	public String getPostForm() {
+		return "page-post-form";
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.POST)
+	public String SavePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		if(result.hasErrors()) {
+			return "redirect:/newpost";
+		}
+		
+		post.setDate(LocalDate.now());
+		blogService.save(post);
+		return "redirect:/posts";
 	}
 
 }
